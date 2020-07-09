487804003d7735d2746590be4314a5575bb3d7f9
erinmayg <erinmayg@users.noreply.github.com>
2020-07-09 23:27:03 +0800

    Added AcidPool.cs Changer.cs and Guide2.cs

---
+++ AcidPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidPool : MonoBehaviour {
+
+    [SerializeField] private int dps = 5;
+    [SerializeField] private AudioSource acidSound;
+    private float currTime = 0;
+
+    private void Update() {
+        currTime -= Time.deltaTime;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (currTime <= 0 && other.gameObject.CompareTag("Player")) {
+            currTime = 1f;
+            other.GetComponent<PlayerStats>().TakeDamage(dps);
+            if (acidSound != null) acidSound.Play();
+        }
+    }
+}
---
+++ Guide2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guide2 : MonoBehaviour {
+
+    [SerializeField] private Dialogue[] dialogues;
+    [SerializeField] private GameObject player;
+    private int i = 0;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        i++;
+        dialogues[i].gameObject.SetActive(true);
+    }
+}
